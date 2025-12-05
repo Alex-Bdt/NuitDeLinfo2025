ed767851e8c754ac41880f48bb86463aef7a5846
Alex <alex.bedet4@gmail.com>
2025-12-05 06:02:56 +0100

    Add files via upload

--- JavaScript/quizz.js
+++ JavaScript/quizz.js
@@ -1,15 +1,63 @@
 const questions = [
     {
-        text: "Quel est le geste numérique le plus efficace pour réduire sa consommation énergétique ?",
-        answers: ["Baisser la luminosité", "Éteindre complètement les appareils"],
+        text: "Prolonger la durée de vie d’un ordinateur permet",
+        answers: ["D’augmenter automatiquement sa vitesse de traitement", "De réduire l’empreinte environnementale liée à sa fabrication et son recyclage"],
         correct: 1,
-        exp: "Éteindre complètement est le geste le plus efficace."
+        exp: "De réduire l’empreinte environnementale liée à sa fabrication et son recyclage"
     },
     {
-        text: "Quel service numérique consomme le plus ?",
-        answers: ["Streaming vidéo", "Recherche Google"],
+        text: "Les GAFAM ont un impact indirect important sur l’environnement car :",
+        answers: ["Ils concentrent énormément de données dans leurs datacenters", "Ils produisent uniquement des logiciels open-source"],
         correct: 0,
-        exp: "Le streaming vidéo est très énergivore."
+        exp: "Ils concentrent énormément de données dans leurs datacenters."
+    },
+    {
+        text: "Pour réduire l’impact énergétique du numérique à petite échelle, il est conseillé de :",
+        answers: ["Supprimer les fichiers inutiles et limiter le stockage en ligne", "Acheter systématiquement le dernier ordinateur disponible"],
+        correct: 0,
+        exp: "Supprimer les fichiers inutiles et limiter le stockage en ligne"
+    },
+    {
+        text: "Le composant le plus problématique à recycler dans un ordinateur est :",
+        answers: ["La souris et le clavier", "L’écran LCD et les batteries"],
+        correct: 1,
+        exp: "L’écran LCD et les batteries"
+    },
+    {
+        text: "Environ quel pourcentage de la consommation énergétique mondiale est liée au numérique ?",
+        answers: ["4 %", "20 %"],
+        correct: 0,
+        exp: "4 %"
+    },
+    {
+        text: "Quelle action quotidienne contribue le plus à réduire l’impact numérique individuel ?",
+        answers: ["Limiter la diffusion et le stockage de vidéos en haute définition", "Ne rien faire"],
+        correct: 0,
+        exp: "Limiter la diffusion et le stockage de vidéos en haute définition"
+    },
+    {
+        text: "Un ordinateur bien entretenu consomme :",
+        answers: ["Moins d’énergie et fonctionne plus longtemps", "Toujours la même énergie, quelle que soit son entretien"],
+        correct: 0,
+        exp: "Moins d’énergie et fonctionne plus longtemps"
+    },
+    {
+        text: "Le streaming vidéo en haute définition :",
+        answers: ["Consomme pas plus que le steaming en basse définition", "Consomme beaucoup d’énergie dans les datacenters et réseaux"],
+        correct: 1,
+        exp: "Consomme beaucoup d’énergie dans les datacenters et réseaux"
+    },
+    {
+        text: "Former les utilisateurs au numérique responsable :",
+        answers: ["Aide à réduire l’impact environnemental du numérique", "A peu d'effet car le matériel informatique reste la principale source d’impact"],
+        correct: 0,
+        exp: "Aide à réduire l’impact environnemental du numérique"
+    },
+    {
+        text: "Choisir des appareils modulables et réparables permet :",
+        answers: ["De réduire uniquement les coûts de réparation, sans impact sur les déchets électroniques", "De réduire les déchets et prolonger la durée de vie"],
+        correct: 1,
+        exp: "De réduire les déchets et prolonger la durée de vie"
     }
 ];
 
--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # NuitDeLinfo2025
 
-echo "# NuitDeLinfo2025" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Alex-Bdt/NuitDeLinfo2025.git
-git push -u origin main
+Projet Nuit de L'info 2025
+
+Un site rétro proposant un quiz éducatif.
+Les questions peuvent être répondues avec la souris et les touches fléchées droites et gauches pour choisir ses réponses.
